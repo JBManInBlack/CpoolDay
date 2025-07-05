bb826bdafe7f58e86272160a6ea553b39daa7fc3
JBManInBlack <jb.ferrer@icloud.com>
2025-07-05 13:17:04 +0000

    le string en taille L

---
+++ main.c
@@ -0,0 +1,7 @@
+void my_print_n_ascii(int n);
+
+int main(void)
+{
+    my_print_revert("je suis magnifique");
+    return 0;
+}
---
+++ my_print_revert.c
@@ -0,0 +1,4 @@
+void my_print_revet(char *toRevert.c);
+{
+    for(i)
+}
