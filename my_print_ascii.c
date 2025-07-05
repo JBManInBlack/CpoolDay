aa1b8acbdb9d99d1c8e06b996e8558c64a8b05bf
JBManInBlack <jb.ferrer@icloud.com>
2025-07-05 11:29:44 +0000

    ca marche mais c obscur

--- my_print_ascii.c
+++ my_print_ascii.c
@@ -1,8 +1,10 @@
+#include "print_char.h"
+
 void my_print_ascii(void)
 {
-    for(int i=0; i<127; i++)
+    for(int i=33; i<127; i++)
     {
-        print_char("%d %c \n",i,i);
+        print_char(i);
     }
 }
 
