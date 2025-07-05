9b88325c9ae9149a163b23ad9b2ab8947b8bcabe
JBManInBlack <jb.ferrer@icloud.com>
2025-07-05 09:58:36 +0000

    ca marche tjr pas

--- my_print_ascii.c
+++ my_print_ascii.c
@@ -1,13 +1,8 @@
 void my_print_ascii(void)
 {
-    for(int i=0, i<127, i++)
+    for(int i=0; i<127; i++)
     {
         print_char("%d %c \n",i,i);
     }
 }
 
-int main(void)
-{
-    my_print_ascii();
-    return 0;
-}
